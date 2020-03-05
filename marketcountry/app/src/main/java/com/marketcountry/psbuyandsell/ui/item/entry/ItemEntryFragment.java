01e987cc1e92bbb750bd842e2a66fb353295e1e7
yhmblq@gmail.com <>
2020-03-05 11:06:30 +0900

    [TaeWoo] Location

--- marketcountry/app/src/main/java/com/marketcountry/psbuyandsell/ui/item/entry/ItemEntryFragment.java
+++ marketcountry/app/src/main/java/com/marketcountry/psbuyandsell/ui/item/entry/ItemEntryFragment.java
@@ -5,9 +5,11 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -38,6 +40,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.marketcountry.psbuyandsell.Config;
+import com.marketcountry.psbuyandsell.GpsTracker;
 import com.marketcountry.psbuyandsell.MainActivity;
 import com.marketcountry.psbuyandsell.R;
 import com.marketcountry.psbuyandsell.binding.FragmentDataBindingComponent;
@@ -54,8 +57,37 @@ import com.marketcountry.psbuyandsell.viewmodel.item.ItemViewModel;
 import com.marketcountry.psbuyandsell.viewobject.Image;
 import com.marketcountry.psbuyandsell.viewobject.Item;
 import com.marketcountry.psbuyandsell.viewobject.common.Resource;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+//
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+//
 
 /**
  * A simple {@link Fragment} subclass.
@@ -105,6 +137,8 @@ public class ItemEntryFragment extends PSFragment implements DataBoundListAdapte
 
     private String typeTemp;
     private String conditionTemp;
+    private GpsTracker gpsTracker;
+
 
     @VisibleForTesting
     private AutoClearedValue<FragmentItemEntryBinding> binding;
@@ -120,7 +154,57 @@ public class ItemEntryFragment extends PSFragment implements DataBoundListAdapte
         setHasOptionsMenu(true);
         initializeMap(savedInstanceState);
 
+        //
+        gpsTracker = new GpsTracker(this.getActivity());
+
+        double latitude = gpsTracker.getLatitude();
+        double longitude = gpsTracker.getLongitude();
+
+        String address = getCurrentAddress(latitude, longitude);
+        binding.get().addressEditText.setText(address);
+
+        Toast.makeText(this.getActivity(), "현재위치 \n위도 " + latitude + "\n경도 " + longitude, Toast.LENGTH_LONG).show();
+
+        //
+
         return binding.get().getRoot();
+    }
+
+
+    public String getCurrentAddress( double latitude, double longitude) {
+
+        //지오코더... GPS를 주소로 변환
+        Geocoder geocoder = new Geocoder(this.getActivity(), Locale.getDefault());
+
+        List<Address> addresses;
+
+        try {
+
+            addresses = geocoder.getFromLocation(
+                    latitude,
+                    longitude,
+                    7);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this.getActivity(), "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this.getActivity(), "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+
+        }
+
+
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this.getActivity(), "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString()+"\n";
+
     }
 
     private void initializeMap(Bundle savedInstanceState) {
@@ -384,6 +468,8 @@ public class ItemEntryFragment extends PSFragment implements DataBoundListAdapte
             }
         }
         //endregion
+
+
     }
 
     private String convertToImagePath(Uri selectedImage, String[] filePathColumn) {
